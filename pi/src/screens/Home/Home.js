d660089a44469576e9958582310bf0566dd332b7
solfernandezbarisic <sfernandezbarisic@udesa.edu.ar>
2026-03-30 16:38:20 -0300

    modifique el home

--- pi/src/screens/Home/Home.js
+++ pi/src/screens/Home/Home.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-
+function Home() {
+    return(
+        <div>
+            <Header/>
+            <Buscador/>
+            <main>
+                <Peliculas/>
+                <Series/>
+            </main>
+        </div>
+    )
+}
 
 export default Home;
