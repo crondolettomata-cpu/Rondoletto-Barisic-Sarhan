2f92b045c423b1aed2365e6c6730e453f723c8b6
crondolettomata-cpu <crondolettomata@udesa.edu.ar>
2026-04-06 17:26:00 -0300

    prueba

---
+++ pi/src/componentes/Loader/Loader.js
@@ -0,0 +1,9 @@
+import React from "react"
+export default function Loader(){
+    return(
+        <div>
+            <h2>Cargando....</h2>
+            <img src="/img/foto cargando.png" alt="Cargando..."/>
+        </div>
+    )
+}
