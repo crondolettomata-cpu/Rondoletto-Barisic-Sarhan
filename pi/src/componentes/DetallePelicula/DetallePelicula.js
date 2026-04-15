0848716d7f3a3ed97afed681412d1b85be01d54f
ysarhan <ysarhan@udesa.edu.ar>
2026-04-14 21:00:31 -0300

    detalle pelicula y detalle serie

--- pi/src/componentes/DetallePelicula/DetallePelicula.js
+++ pi/src/componentes/DetallePelicula/DetallePelicula.js
@@ -1,3 +1,37 @@
 import React from "react";
+import Loader from "../Loader/Loader"
 
+class DetallePelicula extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            datos : null
+        };
+    }
+    componentDidMount (){
+        const pelicula = this.props.match.params.id 
+        fetch("https://api.themoviedb.org/3/movie/${pelicula}?api_key=9f00611fdf617c67427de634a461ac6c")
+        .then (response => response.json())
+        .then (data => {
+            this.setState ({datos: data})
+        })
+        .catch (error => console.log(error))
+    }
+
+    render () {
+        if (!this.state.datos) {
+                return <Loader /> 
+        }
+        return (
+            <section>
+                <img src={`https://image.tmdb.org/t/p/w500${this.state.datos.poster_path}`}> </img>
+                <h1>{this.state.datos.title}</h1>
+                <p>Rating: {this.state.datos.vote_average}</p>
+                <p>Fecha: {this.state.datos.release_date}</p>
+                <p>{this.state.datos.overview}</p>
+                <p>Géneros: {this.state.datos.genres.map(genero => (<li key={genero.id}>{genero.name}</li>))}</p> 
+            </section>
+        )
+    }
+}
 export default DetallePelicula 
--- pi/src/componentes/DetalleSerie/DetalleSerie.js
+++ pi/src/componentes/DetalleSerie/DetalleSerie.js
@@ -1,28 +1,32 @@
 import React, { Component } from "react";
+import Loader from "../Loader/Loader";
 
 class DetalleSerie extends Component {
     constructor (props) {
         super (props);
         this.state = {
-            data: null
+            datos: null
         };
     }
     componentDidMount (){
         const serie = this.props.match.params.id
-       fetch("https://api.themoviedb.org/3/tv/{serie}?api_key=9f00611fdf617c67427de634a461ac6c")
+       fetch("https://api.themoviedb.org/3/tv/${serie}?api_key=9f00611fdf617c67427de634a461ac6c")
        .then(response => response.json())
        .then(data => { console.log(data);
-         this.setState({datos: data.results})})
+         this.setState({datos: data})})
         .catch(error => console.log(error))
     }
 
     render(){
+        if (!this.state.datos){
+            return <Loader />
+        }
         return (
             <section>
                 <img src={`https://image.tmdb.org/t/p/w500${this.state.datos.poster_path}`} alt="" />
                 <h1>{this.state.datos.name}</h1>
                 <p>Rating: {this.state.datos.vote_average}</p>
-                <p>Fecha: {this.state.datos.air_date}</p>
+                <p>Fecha: {this.state.datos.first_air_date}</p>
                 <p>{this.state.datos.overview}</p>
                 <p>Géneros: {this.state.datos.genres.map(genero => (<li key={genero.id}>{genero.name}</li>))}</p>
             </section>
