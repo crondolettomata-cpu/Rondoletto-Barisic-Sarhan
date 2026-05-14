56f108d4afa5bb3d4a9aa30755b18ffa9bac8430
crondolettomata-cpu <crondolettomata@udesa.edu.ar>
2026-05-14 17:11:22 -0300

    modifique el detalleSerie

--- pi/src/componentes/DetalleSerie/DetalleSerie.js
+++ pi/src/componentes/DetalleSerie/DetalleSerie.js
@@ -1,41 +1,49 @@
-import React, { Component } from "react";
+import React, { useState, useEffect} from "react";
 import Loader from "../Loader/Loader";
 import "./styles.css"
 
-class DetalleSerie extends Component {
-    constructor (props) {
-        super (props);
-        this.state = {
-            datos: null
-        };
-    }
-    componentDidMount (){
-        const serie = this.props.match.params.id
-       fetch(`https://api.themoviedb.org/3/tv/${serie}?api_key=9f00611fdf617c67427de634a461ac6c`)
-       .then(response => response.json())
-       .then(data => { console.log(data);
-         this.setState({datos: data})})
-        .catch(error => console.log(error))
-    }
+function DetalleSerie (props) {
+   const [serie, setSerie] = useState(null)
+   useEffect ( () => {
 
-    render(){
-        if (!this.state.datos){
-            return <Loader />
-        }
+        const serie = props.match.params.id
+
+ fetch(`https://api.themoviedb.org/3/tv/${serie}?api_key=9f00611fdf617c67427de634a461ac6c`)
+.then(response => response.json())
+.then(data => setSerie(data))
+.catch(error => console.log(error))
+         
+
+}, [] );
+
+
         return (
-            <section className = "detalleserie">
-                <img className="foto" src={`https://image.tmdb.org/t/p/w500${this.state.datos.poster_path}`} alt="" />
-                <div className="info">
-                    <h1>{this.state.datos.name}</h1>
-                    <p>Rating: {this.state.datos.vote_average}</p>
-                    <p>Fecha: {this.state.datos.first_air_date}</p>
-                    <p>{this.state.datos.overview}</p>
-                    <p>Géneros: {this.state.datos.genres ? this.state.datos.genres.map(genero => genero.name).join(", ") : ""}</p>
+
+            serie ? 
+            <section className="detalleserie"> 
+            <img 
+ 
+src={`https://image.tmdb.org/t/p/w500${serie.poster_path}`} alt={serie.name}  className='foto' />
+            
+             <div className="info">
+                    <h1>{serie.name}</h1>
+                    <p>Rating: {serie.vote_average}</p>
+                    <p>Fecha: {serie.release_data}</p>
+                    <p>{serie.overview}</p>
+                    <p>Duracion: {serie.runtime} minutos</p>
+                    <p>Géneros: {serie.genres ? this.state.datos.genres.map(genero => genero.name).join(", ") : ""}</p>
                 </div>
+           
             </section>
+            : <Loader/>
+
+
         )
-    }
+
 }
+
+
+    
 
 
 export default DetalleSerie;
